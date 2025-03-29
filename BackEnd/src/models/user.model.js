48c950ca997dbd4ee5121eca4902a6cdf8fec008
Mukul Pal <mukulpal0333@gmail.com>
2025-03-29 18:38:42 +0530

    User Model

--- BackEnd/src/models/user.model.js
+++ BackEnd/src/models/user.model.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     name: String,
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password: {
         type: String,
@@ -30,6 +32,13 @@ const userSchema = new mongoose.Schema({
     passwordResetToken: String,
     passwordResetExpiry: Date,
 }, {timestamps: true});
+
+userSchema.pre("save", async function (next) {
+    if(this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10)
+    }
+    next()
+})
 
 const User = mongoose.model("User", userSchema);
 
